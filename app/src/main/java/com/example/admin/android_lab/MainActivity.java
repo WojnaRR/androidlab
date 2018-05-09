95f237154df726ec0dacb39882b6ed4d2abc85ab
WojnaRR <vendetarlz@gmail.com>
2018-05-09 16:51:02 +0200

    zmiana

--- app/src/main/java/com/example/admin/android_lab/MainActivity.java
+++ app/src/main/java/com/example/admin/android_lab/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.android_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),
+                "Kliknięto przycisk FAB",
+                Toast.LENGTH_SHORT).show();
+
     }
 }
