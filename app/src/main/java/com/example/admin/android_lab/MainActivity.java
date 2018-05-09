b13102e8d6a7db70601165a9482e24ab28f99841
WojnaRR <vendetarlz@gmail.com>
2018-05-09 17:02:18 +0200

    Revert: zmiana

--- app/src/main/java/com/example/admin/android_lab/MainActivity.java
+++ app/src/main/java/com/example/admin/android_lab/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.admin.android_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),
-                "Kliknięto przycisk FAB",
-                Toast.LENGTH_SHORT).show();
+        String[] values = new String [] {"pies","kot","gołąb","karp","karaluch","kon","chomik","kon","chomik","kon","chomik","kon","chomik","kon","chomik"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
 
+        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+
+        ListView listview = (ListView) findViewById(R.id.list);
+        listview.setAdapter(this.adapter);
+        Toast.makeText(this,"Kliknięto przycisk Button", Toast.LENGTH_SHORT).show();
     }
 }
